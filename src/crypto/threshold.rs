f1e265aeb2d615ca14988e1ae444f39f52a1e029
Zico <zhicong303@gmail.com>
2022-02-23 22:39:39 +0800

    threshold signature tests pass

--- src/crypto/threshold.rs
+++ src/crypto/threshold.rs
@@ -1,8 +1,9 @@
 use num_bigint::{BigInt, Sign, ToBigInt};
 use miracl_core::bls12381::big::BIG;
 use miracl_core::bls12381::bls::{BLS_OK};
+use miracl_core::bls12381::pair;
 use crate::crypto::bls::Bls;
-use crate::crypto::define::{CURVE_ORDER, MODULUS, MB};
+use crate::crypto::define::{CURVE_ORDER, MODULUS, MB, G1};
 use crate::utils::rand_utils::{RandUtilsRng, Sample};
 use crate::utils::error::{require};
 use crate::math::polynomial::Polynomial;
@@ -24,7 +25,7 @@ impl ThresholdBls {
         }
     }
 
-    pub fn generate(&mut self, sk_shares: &mut [[u8; MB]]) -> isize {
+    pub fn generate(&mut self, sk_shares: &mut [[u8; MB]], ids: &mut [isize]) -> isize {
         let mut rng = RandUtilsRng::new();
 
         self.master_bls.keygen();
@@ -32,12 +33,13 @@ impl ThresholdBls {
         coeffs[0] = BigInt::from_bytes_be(Sign::Plus, &self.master_bls.sk[..]); 
         let poly = Polynomial::new(coeffs);
         for i in 0..self.n {
-            poly.eval(&i.to_bigint().unwrap()).to_BIG().tobytes(&mut sk_shares[i]);
+            poly.eval(&(i+1).to_bigint().unwrap()).to_BIG().tobytes(&mut sk_shares[i]);
+            ids[i] = (i + 1) as isize;
         }
         BLS_OK 
     }
 
-    pub fn aggregate(&self, pk: &[[u8; 2*MB + 1]], sig: &[[u8; MB + 1]], id: &[isize], m: &[u8]) {
+    pub fn aggregate(&self, agg_sig: &mut [u8], pk: &[[u8; 2*MB + 1]], sig: &[[u8; MB + 1]], id: &[isize], m: &[u8]) {
         let mut pk_valid: Vec<[u8; 2*MB+1]> = Vec::new();
         let mut sig_valid: Vec<[u8; MB+1]> = Vec::new();
         let mut id_valid: Vec<isize> = Vec::new();
@@ -49,11 +51,23 @@ impl ThresholdBls {
                 pk_valid.push(pk[i].clone());
                 sig_valid.push(sig[i].clone());
                 id_valid.push(id[i]);
+                if pk_valid.len() >= self.t {
+                    break
+                }
             }
         }
         require(pk_valid.len() >= self.t, "Insufficient valid signatures");
         let mut coeffs = vec![BIG::new_int(0); self.t];
-        Self::lagrange_coeffs(&mut coeffs, id);
+        Self::lagrange_coeffs(&mut coeffs, &id_valid);
+
+        let mut agg = G1::new();
+        agg.inf();
+        for i in 0..self.t {
+            let d = G1::frombytes(&sig[i]);
+            let d = pair::g1mul(&d, &coeffs[i]); 
+            agg.add(&d);
+        }
+        agg.tobytes(agg_sig, true);
     }
 
     fn lagrange_coeffs(coeffs: &mut [BIG], x: &[isize]) {
@@ -61,7 +75,7 @@ impl ThresholdBls {
         let n = coeffs.len();
         for i in 0..n {
             let mut p = BIG::new_int(1);
-            let xi = BIG::new_int(-x[i]);
+            let xi = BIG::modneg(&BIG::new_int(x[i]), &CURVE_ORDER);
             for j in 0..n {
                 if i!=j {
                     let xj = BIG::new_int(x[j]); 
@@ -73,5 +87,21 @@ impl ThresholdBls {
             }
             coeffs[i] = p;
         }
+    }
+
+    pub fn recover_sk(&self, sk: &mut [u8; MB], sk_shares: &[[u8; MB]], id: &[isize]) {
+        require(sk_shares.len() == id.len(), "Different length");
+        require(id.len() >= self.t, "Insufficient shares");
+
+        let mut coeffs = vec![BIG::new_int(0); self.t];
+        Self::lagrange_coeffs(&mut coeffs, &id[0..self.t]);
+
+        let mut s = BIG::new_int(0); 
+        for i in 0..self.t {
+            let share = BIG::frombytes(&sk_shares[i]);
+            let share = BIG::modmul(&share, &coeffs[i], &CURVE_ORDER);
+            s = BIG::modadd(&s, &share, &CURVE_ORDER);
+        }
+        s.tobytes(sk);
     }
 }
---
+++ tests/test_threshold.rs
@@ -0,0 +1,53 @@
+use miracl_core::bls12381::bls::{BLS_OK}; 
+use dvf::crypto::bls::{Bls};
+use dvf::crypto::threshold::{ThresholdBls};
+use dvf::crypto::define::{MB};
+
+
+#[test]
+fn test_threshold() {
+    let t = 5;
+    let n = 10;
+    let mut m_threshold = ThresholdBls::new(t, n);
+    let mut sk_shares = vec![[0 as u8; MB]; n]; 
+    let mut ids = vec![0; n];
+    m_threshold.generate(&mut sk_shares, &mut ids);
+    
+    let mut bls_shares: Vec<Bls> = sk_shares.iter().map(|sk| Bls::from_sk(sk)).collect();
+    let pks: Vec<[u8; 2*MB+1]> = bls_shares.iter().map(|signer| signer.pk).collect();
+    let message = "hello world";
+
+    let mut sigs = vec![[0 as u8; MB+1]; n];
+    for i in 0..n {
+        bls_shares[i].sign(&mut sigs[i], message.as_bytes());
+    }
+    let mut agg_sig: [u8; MB+1] = [0; MB+1];
+    m_threshold.aggregate(&mut agg_sig, &pks, &sigs, &ids, message.as_bytes());
+
+    let mut sig: [u8; MB+1] = [0; MB+1];
+    m_threshold.master_bls.sign(&mut sig, message.as_bytes());
+
+    let status1 = m_threshold.master_bls.verify(&sig, message.as_bytes());
+    let status2 = m_threshold.master_bls.verify(&agg_sig, message.as_bytes());
+    
+    assert_eq!(status1, BLS_OK, "Signature verification failed");
+    assert_eq!(status2, BLS_OK, "Aggregate signature verification failed");
+    assert_eq!(agg_sig, sig, "Signature not match");
+}
+
+#[test]
+fn test_recover_sk() {
+    let t = 5;
+    let n = 10;
+    let mut m_threshold = ThresholdBls::new(t, n);
+    let mut sk_shares = vec![[0 as u8; MB]; n]; 
+    let mut ids = vec![0; n];
+    m_threshold.generate(&mut sk_shares, &mut ids);
+
+    let mut sk = [0 as u8; MB];
+    m_threshold.recover_sk(&mut sk, &sk_shares, &ids);
+
+    assert_eq!(sk, m_threshold.master_bls.sk, "Faile to recover key");
+}
+
+

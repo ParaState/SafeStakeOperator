965610ddf05f0d66fe3a8e299043eeb112eec83e
zico <zhicong303@gmail.com>
2022-11-13 10:41:29 +0000

    dkg

--- src/crypto/dkg.rs
+++ src/crypto/dkg.rs
@@ -7,7 +7,7 @@ use num_bigint::{BigInt, Sign};
 use num_bigint::ToBigInt;
 use crate::math::bigint_ext::Ring;
 use std::sync::Arc;
-use blst::blst_p1;
+use blst::{blst_p1, blst_p1_affine};
 use crate::network::io_committee::{IOCommittee, IOChannel};
 use futures::future::join_all;
 use bytes::Bytes;
@@ -17,7 +17,7 @@ use std::marker::PhantomData;
 
 pub struct DKG<T, U> {
     party: u64,
-    io: T,
+    io: Arc<T>,
     threshold: usize,
     h: blst_p1,
     _phantom: PhantomData<U>,
@@ -28,7 +28,7 @@ where
     U: IOChannel,
     T: IOCommittee<U> {
 
-    pub fn new(party: u64, io: T, threshold: usize) -> Self {
+    pub fn new(party: u64, io: Arc<T>, threshold: usize) -> Self {
         let mut h = std::mem::MaybeUninit::<blst_p1>::uninit();
         let msg = "dvf another generator";
     
@@ -49,52 +49,111 @@ where
         }
     }
 
-    pub async fn run(&self) -> Result<Keypair, DvfError> {
+    pub async fn run(&self) -> Result<(Keypair, PublicKey), DvfError> {
         let mut threshold_sig = ThresholdSignature::new(self.threshold);
         let ids = self.io.ids();
         let (kp, kps) = threshold_sig.key_gen(ids)?;
 
         let kps_ref = &kps; // Take reference so that it can be used in async move
+        let kp_ref = &kp;
         let futs = ids.iter().map(|id| async move {
-            self.io.channel(self.party, *id)
-                .write()
-                .await
-                .send(Bytes::copy_from_slice(kps_ref.get(id).unwrap().sk.serialize().as_bytes())).await;
-            let s = self.io.channel(self.party, *id)
-                .write()
-                .await
-                .recv().await;
+            let mut send_channel = self.io.channel(self.party, *id);
+            let mut recv_channel = self.io.channel(*id, self.party);
+            // Send a share of my secret to party `id`
+            send_channel.send(Bytes::copy_from_slice(kps_ref.get(id).unwrap().sk.serialize().as_bytes())).await;
+            // Recv a share of party `id`'s secret
+            let s = recv_channel.recv().await;
             let s = BigInt::from_bytes_be(Sign::Plus, s.as_ref());
-            (*id, s)
+            // Publish public key corresponding to my secret to party `id`
+            send_channel.send(Bytes::copy_from_slice(kp_ref.pk.serialize().as_slice())).await;
+            // Recv public key corresponding to the secret of party `id`
+            let pk_bytes = recv_channel.recv().await;
+            let mut pk: blst_p1_affine = Default::default();
+            unsafe {
+                blst::blst_p1_uncompress(&mut pk, pk_bytes.as_ptr());
+            };
+
+            (*id, s, pk)
         });
         let results = join_all(futs)
             .await
             .into_iter()
-            .collect::<Vec<(u64, BigInt)>>();
+            .collect::<Vec<(u64, BigInt, blst_p1_affine)>>();
         let mut gsk = 0.to_bigint().unwrap();
-        for (id, s) in results.iter() {
+        // let mut mpk: blst_p1 = Default::default();
+        for (id, s, _) in results.iter() {
             gsk += s;
         }
         gsk = gsk.reduce(&MODULUS);
         let (_, gsk_bytes) = gsk.to_bytes_be();        
         let gsk = SecretKey::deserialize(&gsk_bytes[..]).unwrap();
         let kp = Keypair::from_components(gsk.public_key(), gsk);
-        Ok(kp)
+
+        let pks = results.iter().map(|(_, _, x)| ptr::addr_of!(*x)).collect::<Vec<*const blst_p1_affine>>();
+        let mut mpk_bytes: [u8; PUBLIC_KEY_BYTES_LEN] = [0; PUBLIC_KEY_BYTES_LEN];
+        unsafe {
+            let mut mpk: blst_p1 = Default::default();
+            blst::blst_p1s_add(&mut mpk, pks.as_ptr(), pks.len());
+            blst::blst_p1_compress(mpk_bytes.as_mut_ptr(), &mpk);
+        };
+        let mpk = PublicKey::deserialize(&mpk_bytes).unwrap();
+
+        Ok((kp, mpk))
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use crate::network::io_committee::MemIOCommittee;
+    use crate::crypto::ThresholdSignature;
+    use types::{Hash256}; 
+    use eth2_hashing::{Context, Sha256Context};
+    use futures::future::join_all;
+    use futures::executor::block_on;
+    use bls::{Signature, PublicKey};
 
-    const ids: Vec<u64> = vec![1, 2, 3, 4];
+    const ids: [u64; 4] = [1, 2, 3, 4];
 
     #[test]
     fn test_dkg() {
-        let io = Arc::new(MemIOCommittee::new(ids.as_slice()));
+        let t = 3;
+        // Use dkg to generate secret-shared keys
+        let io = &Arc::new(MemIOCommittee::new(ids.as_slice()));
         let futs = ids.iter().map(|id| async move {
-            let mut dkg = DKG::new(id, io);
-            (id, dkg.run().await)
+            let mut dkg = DKG::new(*id, io.clone(), t);
+            dkg.run().await
         });
+
+        let results = block_on(join_all(futs))
+            .into_iter()
+            .flatten()
+            .collect::<Vec<(Keypair, PublicKey)>>();
+        
+        // Verify master public keys
+        for i in 1..results.len() {
+            assert_eq!(results[i].1, results[i-1].1, "Master public keys are not the same");
+        }
+
+        // Sign something with the shared keys
+        let kps: Vec<Keypair> = results.iter().map(|(kp, _)| kp.clone()).collect();
+        let pks: Vec<&PublicKey> = kps.iter().map(|kp| &kp.pk).collect();
+        let message = "hello world";
+        let mut context = Context::new();
+        context.update(message.as_bytes());
+        let message = Hash256::from_slice(&context.finalize());
+
+        let mut sigs: Vec<Signature> = Vec::new();
+        for kp in kps.iter() {
+            sigs.push(kp.sk.sign(message));
+        }
+        let sigs_ref: Vec<&Signature> = sigs.iter().map(|s| s).collect();
+        let mut m_threshold = ThresholdSignature::new(t);
+        let agg_sig = m_threshold.threshold_aggregate(&sigs_ref[..], &pks[..], &ids[..], message).unwrap();
+
+        // Verification with the master public key
+        let status1 = agg_sig.verify(&results[0].1, message);
+        
+        assert!(status1, "Signature verification failed");
     }
 }
--- src/network/io_committee.rs
+++ src/network/io_committee.rs
@@ -5,23 +5,31 @@ use std::collections::HashMap;
 use bytes::Bytes;
 use std::sync::Arc;
 use tokio::sync::{RwLock};
+use futures::stream::{SplitSink, SplitStream};
+use futures::stream::StreamExt as _;
+use tokio::net::{TcpListener, TcpStream};
+use tokio_util::codec::{Framed, LengthDelimitedCodec};
+use futures::SinkExt;
+use std::net::{SocketAddr, IpAddr};
+use log::{info, warn, error, debug};
+use tokio::task::JoinHandle;
 
 #[async_trait]
 pub trait IOChannel {
-    async fn send(&mut self, message: Bytes);
-    async fn recv(&mut self) -> Bytes;
+    async fn send(&self, message: Bytes);
+    async fn recv(&self) -> Bytes;
 }
 
 pub struct MemIOChannel {
     sender: Sender<Bytes>,
-    receiver: Receiver<Bytes>,
+    receiver: Arc<RwLock<Receiver<Bytes>>>,
 }
 
 impl MemIOChannel {
     pub fn new(sender: Sender<Bytes>, receiver:Receiver<Bytes>) -> Self {
         Self {
             sender,
-            receiver,
+            receiver: Arc::new(RwLock::new(receiver)),
         }
     }
 }
@@ -29,14 +37,15 @@ impl MemIOChannel {
 #[async_trait]
 impl IOChannel for MemIOChannel {
 
-    async fn send(&mut self, message: Bytes) {
+    async fn send(&self, message: Bytes) {
         if let Err(e) = self.sender.send(message).await {
             panic!("Failed to send message. Error: {}", e);
         }
     }
 
-    async fn recv(&mut self) -> Bytes {
-        if let Some(message) = self.receiver.recv().await {
+    async fn recv(&self) -> Bytes {
+        let mut receiver = self.receiver.write().await;
+        if let Some(message) = receiver.recv().await {
             message
         }
         else {
@@ -45,25 +54,117 @@ impl IOChannel for MemIOChannel {
     }
 }
 
+pub struct ConnectionManager {
+    /// Address to listen to.
+    address: SocketAddr,
+    /// Struct responsible to define how to handle received messages.
+    connections: Arc<RwLock<HashMap<IpAddr, TcpStream>>>,
+    thread_handle: JoinHandle<()>,
+}
+
+impl Drop for ConnectionManager {
+    fn drop(&mut self) {
+        info!("Shutting down connection manager");
+        self.thread_handle.abort();
+    }
+}
+
+impl ConnectionManager {
+    pub fn new(address: SocketAddr) -> Self {
+        let connections: Arc<RwLock<HashMap<IpAddr, TcpStream>>> = 
+            Arc::new(RwLock::new(HashMap::default()));
+        let connections_clone = connections.clone();
+        let address_clone = address.clone();
+        let thread_handle = tokio::spawn(async move {
+            let listener = TcpListener::bind(&address_clone)
+                .await
+                .expect(format!("Failed to bind TCP address {}", address_clone).as_str());
+
+            info!("Listening on {}. [DKG]", address_clone);
+            loop {
+                let (socket, peer) = match listener.accept().await {
+                    Ok(value) => value,
+                    Err(e) => {
+                        warn!("Failed to accept connection: {}", e);
+                        continue;
+                    }
+                };
+                let mut connections = connections_clone.write().await;
+                connections.insert(peer.ip(), socket);
+            }
+        });
+
+        Self {
+            address,
+            connections,
+            thread_handle,
+        }
+    }
+
+    pub async fn acquire_connection(&mut self, ip: IpAddr) -> Option<TcpStream> {
+        self.connections.write().await.remove(&ip)
+    }
+}
+
+type Writer = SplitSink<Framed<TcpStream, LengthDelimitedCodec>, Bytes>;
+type Reader = SplitStream<Framed<TcpStream, LengthDelimitedCodec>>;
+
+pub struct NetIOChannel {
+    writer: Arc<RwLock<Writer>>,
+    reader: Arc<RwLock<Reader>>,
+}
+
+impl NetIOChannel {
+    pub fn new(socket: TcpStream) -> Self {
+        let transport = Framed::new(socket, LengthDelimitedCodec::new());
+        let (writer, reader) = transport.split();
+        Self {
+            writer: Arc::new(RwLock::new(writer)),
+            reader: Arc::new(RwLock::new(reader)),
+        }
+    }
+}
+
+#[async_trait]
+impl IOChannel for NetIOChannel {
+
+    async fn send(&self, message: Bytes) {
+        let mut writer = self.writer.write().await;
+        if let Err(e) = writer.send(message).await {
+            panic!("Failed to send message. Error: {}", e);
+        }
+    }
+
+    async fn recv(&self) -> Bytes {
+        let mut reader = self.reader.write().await;
+        if let Some(Ok(message)) = reader.next().await {
+            message.freeze()
+        }
+        else {
+            panic!("Failed to recv message.");
+        }
+    }
+}
+
 pub trait IOCommittee<T> {
-    fn channel(&self, from: u64, to: u64) -> Arc<RwLock<T>>;
+    fn channel(&self, from: u64, to: u64) -> &T;
     fn ids(&self) -> &[u64];
 }
 
 pub struct MemIOCommittee {
     ids: Vec<u64>,
-    channels: HashMap<u64, HashMap<u64, Arc<RwLock<MemIOChannel>>>>,
+    channels: HashMap<u64, HashMap<u64, MemIOChannel>>,
 }
 
 impl MemIOCommittee {
     pub fn new(ids: &[u64]) -> MemIOCommittee {
-        let mut channels: HashMap<u64, HashMap<u64, Arc<RwLock<MemIOChannel>>>> = Default::default();
+        let mut channels: HashMap<u64, HashMap<u64, MemIOChannel>> = Default::default();
         let n = ids.len();
         for i in 0..n {
             channels.insert(ids[i], HashMap::default());
             for j in 0..n {
                 let (sender, receiver) = channel(DEFAULT_CHANNEL_CAPACITY);
-                channels.get_mut(&ids[i]).unwrap().insert(ids[j], Arc::new(RwLock::new(MemIOChannel::new(sender, receiver))));
+                channels.get_mut(&ids[i]).unwrap().insert(ids[j], MemIOChannel::new(sender, receiver));
             }
         }
         Self {
@@ -79,12 +180,50 @@ impl IOCommittee<MemIOChannel> for MemIOCommittee {
         self.ids.as_slice()
     }
 
-    fn channel(&self, from: u64, to: u64) -> Arc<RwLock<MemIOChannel>> {
+    fn channel(&self, from: u64, to: u64) -> &MemIOChannel {
         self.channels
             .get(&from)
             .unwrap()
             .get(&to)
             .unwrap()
-            .clone()
+    }
+}
+
+/// Network IO committee
+pub struct NetIOCommittee {
+    ids: Vec<u64>,
+    channels: HashMap<u64, HashMap<u64, NetIOChannel>>,
+}
+
+impl NetIOCommittee {
+    pub fn new(ids: &[u64], addresses: &[SocketAddr]) -> MemIOCommittee {
+        let mut channels: HashMap<u64, HashMap<u64, NetIOChannel>> = Default::default();
+        let n = ids.len();
+        for i in 0..n {
+            channels.insert(ids[i], HashMap::default());
+            for j in 0..n {
+                let (sender, receiver) = channel(DEFAULT_CHANNEL_CAPACITY);
+                channels.get_mut(&ids[i]).unwrap().insert(ids[j], MemIOChannel::new(sender, receiver));
+            }
+        }
+        Self {
+            ids: ids.to_vec(),
+            channels,
+        }
+    }
+}
+
+impl IOCommittee<MemIOChannel> for MemIOCommittee {
+
+    fn ids(&self) -> &[u64] {
+        self.ids.as_slice()
+    }
+
+    fn channel(&self, from: u64, to: u64) -> &MemIOChannel {
+        self.channels
+            .get(&from)
+            .unwrap()
+            .get(&to)
+            .unwrap()
     }
 }

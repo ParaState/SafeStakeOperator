f42bb73a17e0b609bdce8a042ecbf372c42ea373
jianlin jiang <jianlinjiang@whu.edu.cn>
2023-01-13 01:00:39 +0800

    remove old data

--- update_record.sh
+++ update_record.sh
@@ -1,3 +1,5 @@
+rm -rf ../data/prater/secrets/
+rm -rf ../data/prater/validators/
 rm -rf ../data/prater/contract_store/
 rm -rf ../data/prater/contract_database.db
 echo "---\nblock_num: 8291298" > ../data/prater/contract_record.yml

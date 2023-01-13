5eb4fa1d1422f169c387993076ebf0082579d13b
jianlin jiang <jianlinjiang@whu.edu.cn>
2023-01-13 19:37:36 +0800

    add debug log

--- src/validation/signing_method.rs
+++ src/validation/signing_method.rs
@@ -335,8 +335,9 @@ impl SigningMethod {
                                             duty: duty.to_string(),
                                             time: Utc::now().signed_duration_since(dt).num_milliseconds()
                                         };
-                                        log::info!("[Dvf Request]: {:?}", &request_body);
+                                        log::info!("[Dvf Request] Body: {:?}", &request_body);
                                         let url_str = API_ADDRESS.get().unwrap().to_owned() + COLLECT_PERFORMANCE_URL;
+                                        log::info!("[Dvf Request] Url: {}", url_str);
                                         request_to_web_server(request_body, &url_str).await.map_err(|e| Error::Web3SignerRequestFailed(e))?;
                                     }
                                     Ok(signature)

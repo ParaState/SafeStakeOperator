2f2448f7806dec33e1702c6e404247ca95439b3a
jianlin jiang <jianlinjiang@whu.edu.cn>
2023-01-14 22:08:38 +0800

    update record

--- update_record.sh
+++ update_record.sh
@@ -2,4 +2,4 @@ rm -rf ../data/prater/secrets/
 rm -rf ../data/prater/validators/
 rm -rf ../data/prater/contract_store/
 rm -rf ../data/prater/contract_database.db
-echo "---\nblock_num: 8291298" > ../data/prater/contract_record.yml
+echo "---\nblock_num: 8310214" > ../data/prater/contract_record.yml

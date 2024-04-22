563862716636afa812f5c021a464b259c6206d62
jianlin jiang <jianlinjiang@whu.edu.cn>
2024-04-22 23:01:00 +0800

    add log

--- src/bin/dvf_root_node.rs
+++ src/bin/dvf_root_node.rs
@@ -166,7 +166,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
                         }
                     },
                     Event::SessionEstablished(enr,  _addr) => {
+
                         if let Some(enr_ip) =  enr.ip4() {
+                            info!("A peer has established session: public key: {}, port: {:?}",
+                                base64::encode(enr.public_key().encode()), enr.udp4());
                             if let Some(discv_port) = enr.udp4() {
                                 let socketaddr = SocketAddr::new(IpAddr::V4(enr_ip), discv_port - DISCOVERY_PORT_OFFSET);
                                 info!("A peer has established session: public key: {}, base addr: {:?}",

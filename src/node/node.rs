312f0410068fd4e06079258db79067b281491aa4
jiangjianlin <jianlinjiang@whu.edu.cn>
2024-06-18 21:49:46 +0800

    monitor ip from boot node

--- src/node/node.rs
+++ src/node/node.rs
@@ -397,7 +397,7 @@ impl<T: EthSpec> Node<T> {
                         // Query IP for each operator in this committee. If any of them changed, should restart the VA.
                         let mut restart = false;
                         for i in 0..committee_def.node_public_keys.len() {
-                            if let Some(addr) = node_lock.discovery.query_addr(&committee_def.node_public_keys[i].0).await {
+                            if let Some(addr) = node_lock.discovery.query_addr_from_boot(0, &committee_def.node_public_keys[i].0).await {
                                 if let Some(socket) = committee_def.base_socket_addresses[i].as_mut() {
                                     if *socket != addr {
                                         info!("op id: {}, local committee definition address {}, queried result {}", committee_def.operator_ids[i], socket, addr);

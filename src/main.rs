b7aa0c20c1fcaae49e09c27f886f56b21a06a470
jianlinjiang <jianlinjiang@whu.edu.cn>
2025-10-28 06:57:54 +0000

    fix logs

--- src/main.rs
+++ src/main.rs
@@ -589,7 +589,7 @@ fn run<E: EthSpec>(
     logging_layers.push(
         stdout_logging_layer
             .with_filter(logger_config.debug_level)
-            .with_filter(workspace_filter.clone())
+            // .with_filter(workspace_filter.clone())
             .boxed(),
     );
 
@@ -597,7 +597,7 @@ fn run<E: EthSpec>(
         logging_layers.push(
             file_logging_layer
                 .with_filter(logger_config.logfile_debug_level)
-                .with_filter(workspace_filter.clone())
+                // .with_filter(workspace_filter.clone())
                 .boxed(),
         );
     }
